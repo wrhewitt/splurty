f074acbebebb97701a99bcf5197daf0fb118e456
wrhewitt <rhewitt@ebsco.com>
2019-09-13 16:13:17 -0500

    replacing new quote endpoint and page with modal

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -2,9 +2,7 @@ class QuotesController < ApplicationController
     def index 
         @quote = Quote.order("RANDOM()").first
     end
-    def new
-        @quote = Quote.new
-    end
+ 
 
     def create
         @quote = Quote.create(quote_params)
